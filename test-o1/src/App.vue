7ea0844ecb372a3b458e903e341a3d2645e01408
Dean Leigh <dean.leigh@wholething.co.uk>
2020-08-03 20:21:12 +0100

    Added Header

--- test-o1/src/App.vue
+++ test-o1/src/App.vue
@@ -7,11 +7,13 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
